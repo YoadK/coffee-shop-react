e499370b482e1e0b341a0ead9d9fae01360ed2c0
YoadK <yoadkot@gmail.com>
2025-08-25 01:03:11 +0300

    modifications were done to 'app.css', 'layout.tsx'

--- client/src/Components/Layout/Layout.tsx
+++ client/src/Components/Layout/Layout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-// import Navbar from "./Navbar";
+import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-import "./Layout.css";
+
 export default function Layout() {
   return (
     <div className="app-shell">
-      {/* <Navbar /> */}
+      <Navbar />
       <main className="container" role="main">
         <Outlet />
       </main>
